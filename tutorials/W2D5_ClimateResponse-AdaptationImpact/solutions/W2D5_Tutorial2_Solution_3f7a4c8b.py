ccca43e5885959448b669c7e0d70206484c71178
Wesley Banfield <wesleybanfield@gmail.com>
2023-07-04 10:36:22 +0200

    all notebooks executed

---
+++ tutorials/W2D5_ClimateResponse-AdaptationImpact/solutions/W2D5_Tutorial2_Solution_3f7a4c8b.py
@@ -0,0 +1,4 @@
+"""
+1. The plot above shows that the min_air_temp (minimum air temperature) has the highest importance score, followed by week of year and tdtr (diurnal temperature range). Warmer environments are more conducive to mosquito populations, and the week of year may be important due to the season.
+2. Surprisingly, these results suggest that some of the precipitation variables were not very helpful. One variable that might be more relevant to mosquito populatoins is an esimate of standing water.
+""";
---
+++ tutorials/W2D5_ClimateResponse-AdaptationImpact/solutions/W2D5_Tutorial3_Solution_f163bfa3.py
@@ -0,0 +1,3 @@
+"""
+1. The recall score is higher, suggesting that the model is good at capturing the true cropland, but precision is lower, indicating it does classify non-crop land as cropland.
+""";
