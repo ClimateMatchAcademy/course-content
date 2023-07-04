ece408d01339aa727b415b16df2966aad6478cf6
Wesley Banfield <wesleybanfield@gmail.com>
2023-07-04 20:35:43 +0200

    run most books except w2d1 and w1d5

---
+++ tutorials/W1D5_ClimateModeling/solutions/W1D5_Tutorial3_Solution_e2b68dd6.py
@@ -0,0 +1,5 @@
+
+"""
+1. We could heat the entire atmosphere by 1 degree with the energy required to raise the upper 2 and a half meters of the ocean by 1 degree.
+   Relative to the atmosphere, the ocean can take in an enormous amount of energy before raising it's temperature appreciably.
+""";
