2ec8bfbec51b88dddca6ee2e013677cfacd6a21f
Wesley Banfield <wesleybanfield@gmail.com>
2023-07-05 09:53:54 +0000

    w1d5 and w2d1 run

---
+++ tutorials/W1D5_ClimateModeling/solutions/W1D5_Tutorial5_Solution_4724967d.py
@@ -0,0 +1,5 @@
+
+"""
+1. One thing we are currently lacking is physical processes (aside from radiation) in our model.
+2. This is a hard question to answer! Luckily we can remove gases one at a time from our model to study their individual impact. Any ideas you have here can be tested with this model!
+""";
---
+++ tutorials/W2D1_FutureClimate-IPCCIPhysicalBasis/solutions/W2D1_Tutorial_2_Solution_b0a7a57f.py
@@ -0,0 +1,4 @@
+
+"""
+1. The plots show warming of the ocean surface under SSP1-2.6, and the even greater warming seen under SSP5-8.5. This is expected from the forcing, the emissions under both scenarios lead to an increase in radiative forcing later in the century (which warms the planet), with the highest radiative forcing and warming under the SSP5-8.5 scenario.
+"""
