4c264351b322ce1d08d11ca690d6f1efc481dbe7
Wesley Banfield <wesleybanfield@gmail.com>
2023-07-06 11:10:35 +0200

    Fixes (#146)

--- tutorials/W2D5_ClimateResponse-AdaptationImpact/solutions/W2D5_Tutorial3_Solution_5348d632.py
+++
@@ -1,8 +0,0 @@
-from sklearn.inspection import permutation_importance
-
-perm_feat_imp = permutation_importance(trained_model, X_test,y_test,
-                           n_repeats=10,
-                           random_state=0)
-
-
-plot_permutation_feature_importance(perm_feat_imp, X_test)
