bce66c48a496b17ef05add2c7c47838428595297
Wesley Banfield <wesleybanfield@gmail.com>
2023-07-13 17:02:56 +0200

    fix w1d1

---
+++ tutorials/W1D1_ClimateSystemOverview/solutions/W1D1_Tutorial2_Solution_0e1187c0.py
@@ -0,0 +1,3 @@
+
+coordinate_selection = temperature.sel(lat="25.0", lon="-120.0")
+coordinate_selection
---
+++ tutorials/W1D1_ClimateSystemOverview/solutions/W1D1_Tutorial8_Solution_0bc6741a.py
@@ -0,0 +1,7 @@
+
+"""
+1. Masking in climate data analysis allows us to focus on specific portions of data that meet particular criteria. By "masking," or hiding values that do not meet these criteria, we can isolate and visualize parts of the data that matter most to our research questions.
+2. In the Antarctic, they are roughly the same. In the Artic, there is more area with warmer SST in 2014.
+3. The ice-albedo feedback could be amplifying the ocean warming noted in Question 2. The polar regions examined here are the only places where waters can be cold enough to sustain sea ice. In a warming world where sea ice is melting, areas of the ocean that were once covered by reflective ice are replaced by darker water, which absorbs more sunlight, warms and hinders sea-ice growth.
+
+"""
