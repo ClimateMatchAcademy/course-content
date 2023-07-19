6be90b47391803d4153b6a276ef71b9aa01ec748
GitHub Action <action@github.com>
2023-07-19 21:14:27 +0000

    Process tutorial notebooks

--- tutorials/W2D1_FutureClimate-IPCCIPhysicalBasis/solutions/W2D1_Tutorial_1_Solution_f9a48a4e.py
+++
@@ -1,4 +0,0 @@
-
-"""
-1. They look similar, which is what we expect from the regridding operation. It should not significantly change the underlying spatial information (i.e., the data), it should just adjust the locations at which that information is provided.
-""";
--- tutorials/W2D1_FutureClimate-IPCCIPhysicalBasis/solutions/W2D1_Tutorial_5_Solution_6d329709.py
+++
@@ -1,4 +0,0 @@
-
-"""
-1. The uncertainty of the single-model ensemble is much smaller than the uncertainty of the multi-model ensemble. The single-model ensemble variations are driven only by internal climate variability, whereas the multi-model ensemble includes inter-model differences (numerics, physics, discretization) in addition to internal climate variability. Comparing the two plots suggests that most of our uncertainty in future projections of global mean SST come from inter-model differences. Internal climate variability is a relatively small contributor to this uncertainty.
-""";
