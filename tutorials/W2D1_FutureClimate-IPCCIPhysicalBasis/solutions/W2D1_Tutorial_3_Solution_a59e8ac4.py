5bd728f11f52e4685d086e20324d120deaee5700
Jenna Pearson <jennalynnpearson@gmail.com>
2023-07-20 02:06:30 -0700

    W2d1 course updates and student notebooks (#218)

--- tutorials/W2D1_FutureClimate-IPCCIPhysicalBasis/solutions/W2D1_Tutorial_3_Solution_a59e8ac4.py
+++
@@ -1,5 +0,0 @@
-
-"""
-1. The models agree much better across this time period, particularly in the reference period and during the rapid warming phase projected in the near future.
-2. There is variability at both ends of the scale, but the future (2080) period has more inter-model variability than the past (1850) period. This is likely because each of these models has a simulates a unique climate system, as we found in the last question, which leads to different responses to the rapid  changes in effective radiative forcing that are projected in the near future.
-""";
--- tutorials/W2D1_FutureClimate-IPCCIPhysicalBasis/solutions/W2D1_Tutorial_4_Solution_0effd963.py
+++
@@ -1,6 +0,0 @@
-
-"""
-1. Under both of these scenarios (SSP1-2.6 and SSP5-8.5), the projected warming of the climate is greater than the multi-model uncertainty.
-2. The warming is greater for SSP5-8.5, but so is the multi-model uncertainty, so the relative size of physical changes and multi-model uncertainty is comparable across both scenarios.
-3. The likely range includes the middle 66% of model results at a given time. For 5 models this means the ranges encompass the middle 3 models (i.e., they exclude the highest and lowest cvalued model at each time).
-""";
